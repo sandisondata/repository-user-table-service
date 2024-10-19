e5093c9274a7261c76e513fb454afc17e7f14e83
Grant Fortington <gwfortington@hotmail.com>
2024-10-19 20:56:01 +1030

    development

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.service = void 0;
 const class_1 = require("./class");
-const service = new class_1.Service('system-user-table-service', '_user_tables', ['user_uuid', 'table_uuid'], ['can_create', 'can_read', 'can_update', 'can_delete'], false);
-exports.service = service;
+exports.service = new class_1.Service('system-user-table-service', '_user_tables', ['user_uuid', 'table_uuid'], ['can_create', 'can_read', 'can_update', 'can_delete'], false);
